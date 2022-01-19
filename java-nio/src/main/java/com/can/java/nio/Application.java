7897ad5703f13e9753d74a9fb8fa38e18772bb11
lcn29 <canlee529@gmail.com>
2022-01-19 17:54:26 +0800

    project init

---
+++ java-nio/src/main/java/com/can/java/nio/Application.java
@@ -0,0 +1,16 @@
+package com.can.java.nio;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author
+ * @date 2022-01-19  17:19
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("OK");
+	}
+}
